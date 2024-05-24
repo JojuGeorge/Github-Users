f27d7f690566a8282c5787881e94ca6107f1fccc
JGeorge53 <Joju.George53@T-Mobile.com>
2024-05-24 11:38:33 +0530

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 import {Dashboard, Login, Error} from './pages'
 
 function App() {
   return (
     <div className="App">
-      <Dashboard></Dashboard>
-      <Login/>
-      <Error/>
+      <Routes>
+        <Route path='/' exact={true} element={<Dashboard/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+      {/* <Dashboard></Dashboard> */}
+       {/* <Login/> */}
+      {/* <Error/> */}
     </div>
   );
 }
