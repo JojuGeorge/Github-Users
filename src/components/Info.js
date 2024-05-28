aa570f6adf2c40942dddc4d41989fc7b06719cda
JGeorge53 <Joju.George53@T-Mobile.com>
2024-05-28 13:07:42 +0530

    added axios, info component updated with mock data

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,62 @@
 import React, {useContext} from 'react'
 import { GithubContext } from '../context/context'
+import {GoRepo, GoCodeSquare} from 'react-icons/go';
+import {FiUserPlus, FiUsers, FiuserPlus} from 'react-icons/fi';
+
+
 
 function Info() {
-  const data = useContext(GithubContext);
-  console.log(data)
+  const {githubUser} = useContext(GithubContext);
+
+  const {public_repos, followers, following, public_gists} = githubUser;
+  
+  console.log(githubUser)
+  console.log(public_repos, followers, following, public_gists)
+
+  const items = [
+    {
+      id:1,
+      icon: <GoRepo/>,
+      label: 'repos',
+      value: public_repos,
+    },{
+      id:2,
+      icon: <FiUsers/>,
+      label: 'followers',
+      value: followers,
+    },{
+      id:3,
+      icon: <FiUserPlus/>,
+      label: 'following',
+      value: following,
+    },{
+      id:4,
+      icon: <GoCodeSquare/>,
+      label: 'gists',
+      value: public_gists,
+    }
+  ]
+
   return (
-    <div>Info </div>
+    <section>
+      <div>
+        {
+          items.map(item => {
+            return <Item key={item.id} {...item}></Item>
+          })
+        }
+      </div>
+    </section>
+  )
+}
+
+const Item = ({icon, label, value})=>{
+  return (
+    <article>
+      <span>{icon}</span>
+      <h3>{value}</h3>
+      <p>{label}</p>
+    </article>
   )
 }
 
