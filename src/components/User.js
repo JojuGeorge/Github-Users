72c759e0a4c0306fe23f97a83b327951bfe7e7af
JGeorge53 <Joju.George53@T-Mobile.com>
2024-05-28 16:56:19 +0530

    basic user card details added, basic bootstrap styling

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import Card from './Card'
+import UserCard from './UserCard'
 import Followers from './Followers'
+import {Row, Col} from 'react-bootstrap'
+
 
 function User() {
+
   return (
     <div>
-      <Card></Card>
-      <Followers></Followers>
+      <Row>
+        <Col className='card' >
+           <UserCard></UserCard>
+        </Col>
+        <Col className='card'>
+          <Followers></Followers>
+        </Col>
+      </Row>
     </div>
   )
 }
---
+++ src/components/UserCard.js
@@ -0,0 +1,43 @@
+import React, {useState, useContext}  from 'react'
+import { GithubContext } from '../context/context'
+import {Row, Col} from 'react-bootstrap'
+import {MdBusiness, MdLocationOn, MdLink} from 'react-icons/md'
+
+function UserCard() {
+  const {githubUser} = useContext(GithubContext);
+
+  const {
+    avatar_url, 
+    html_url, 
+    name,
+    company, 
+    blog, 
+    bio, 
+    location, 
+    twitter_username,
+  } = githubUser;
+
+  return (
+    <div>  
+      <div>
+        <img src={avatar_url} alt={name} className="img-fluid img-thumbnail rounded-circle mb-2"
+                  style={{width: '80px', height: '80px'}}
+                  />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'John Doe'}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </div>
+      <p>{bio}</p>
+
+      <div>
+        <p> <MdBusiness/>{company || 'indie'} </p>
+        <p> <MdLocationOn/> {location || 'earth'} </p>
+        <a href={`https://${blog}`}> <MdLink/> {blog} </a>
+      </div>
+     </div>
+  )
+}
+
+export default UserCard
