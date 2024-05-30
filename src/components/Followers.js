e29b1f279a5b273e057cb398823543a554d49da0
JGeorge53 <Joju.George53@T-Mobile.com>
2024-05-30 10:52:33 +0530

    working on followers

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GithubContext} from '../context/context'
+import {Row, Col} from 'react-bootstrap'
 
 function Followers() {
+  const {followers} = useContext(GithubContext);
+
   return (
-    <div>Followers</div>
+    <div>
+      <div>
+        {followers.map((follower, index )=> {
+           const {avatar_url, login, html_url} = follower;
+
+           return(
+            <div key={index}>
+              <img src={avatar_url} alt={login}/>
+              <span>
+                <h4>{login}</h4>
+                <a href={html_url}>{html_url}</a>
+              </span>
+            </div>
+           )
+        })}
+      </div>
+    </div>
   )
 }
+
+
 
 export default Followers
