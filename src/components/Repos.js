3836220cd51fc58bccc8273d07caf75746f533da
JojuGeorge <jojugeorge001@gmail.com>
2024-05-30 16:14:13 +0530

    3d pie chart added

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,8 +4,34 @@ import { GithubContext } from "../context/context";
 
 function Repos() {
   const { repos } = useContext(GithubContext);
-  console.log(repos);
-  return <div>Repos</div>;
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+
+    if (!language) return total; // removes null values
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  return (
+    <div>
+      <section>
+        <Pie3D data={languages}></Pie3D>
+      </section>
+    </div>
+  );
 }
 
 export default Repos;
