b960cbeceb733e1beec66103817564f052385463
JojuGeorge <jojugeorge001@gmail.com>
2024-05-31 18:46:46 +0530

    follower card style done, dashboard basic style done

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,37 +1,34 @@
 import React, { useContext } from "react";
 import { GithubContext } from "../context/context";
 import { Row, Col } from "react-bootstrap";
+import "../styles/css/followers.css";
 
 function Followers() {
   const { followers } = useContext(GithubContext);
 
   return (
     <div>
-      <div
-        className="overflow-scroll"
-        style={{ height: "300px", overflow: "hidden" }}
-      >
+      <div className="overflow-scroll">
         {followers.map((follower, index) => {
           const { avatar_url, login, html_url } = follower;
 
           return (
-            <div key={index}>
-              <Row>
-                <Col>
+            <div key={index} className="follower-wrapper">
+              <span className="follower-row">
+                <span className="follower-col">
                   <img
                     src={avatar_url}
                     alt={login}
                     className="img-fluid img-thumbnail rounded-circle mb-2"
-                    style={{ width: "80px", height: "80px" }}
                   />
-                </Col>
-                <Col>
+                </span>
+                <span className="follower-col">
                   <span>
-                    <h4>{login}</h4>
+                    <h5>{login}</h5>
                     <a href={html_url}>{html_url}</a>
                   </span>
-                </Col>
-              </Row>
+                </span>
+              </span>
             </div>
           );
         })}
