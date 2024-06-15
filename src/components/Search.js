37b9b7332aaac75c3535aec12b5a58e0edad45e9
JojuGeorge <jojugeorge001@gmail.com>
2024-06-15 17:20:44 +0530

    search bar added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Search() {
+  const [user, setUser] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user) {
+    }
+  };
+
   return (
-    <div>Search</div>
-  )
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            placeholder="Enter Github User"
+          />
+          <button type="submit">Search</button>
+        </form>
+        <h3>requests : 60 / 60</h3>
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
