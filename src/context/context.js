59bce810656c7f6a7457715e0526010044b3003c
JGeorge53 <Joju.George53@T-Mobile.com>
2024-06-17 12:33:44 +0530

    dynamic user, follower, repo data using API

--- src/context/context.js
+++ src/context/context.js
@@ -26,6 +26,14 @@ const GithubProvider = ({children})=>{
 
         if(response){
             setGithubUser(response.data);
+
+            const {login, followers_url} = response.data;
+
+            axios(`${rootUrl}/users/${login}/repos?per_page=100`)
+            .then(response=> setRepos(response.data))
+
+            axios(`${followers_url}?per_page=100`)
+            .then(response => setFollowers(response.data))
         }else{
             toggleError(true, "There is no user with that userName")
         }
