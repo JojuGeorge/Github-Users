114b8019eda86fb7afb217c33ddb86ddadf688c1
JGeorge53 <Joju.George53@T-Mobile.com>
2024-06-27 15:38:34 +0530

    basic auth 0 working done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,27 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
   const {isAuthenticated, loginWithRedirect, logout, user, isLoading} = useAuth0();
+  const isUser = isAuthenticated && user;
+  
   console.log(isAuthenticated, user, isLoading)
   console.log(window.location.origin)
-  console.log(loginWithRedirect)
   return (
     <div>
       <div>
-        <button onClick={loginWithRedirect}>login</button>
-        <button  onClick={() =>
-                  logout({
-                    logoutParams: {
-                      returnTo: `${window.location.origin}/Github-Users/#`
-                    }
-                  })
-                }>logout</button>
+        {isUser && user.picture &&  <img src={user.picture} alt={user.name}/>}
+        {isUser && user.name && <h4>Welcome, <strong>{user.name.toLocaleUpperCase()}</strong></h4>}
+        {isUser ? (
+          <button  onClick={() =>
+            logout({
+              logoutParams: {
+                returnTo: `${window.location.origin}/Github-Users/#`
+              }
+            })
+          }>logout</button>
+        ):(
+          <button onClick={loginWithRedirect}>login</button>
+        )}
+ 
       </div>
     </div>
   )
